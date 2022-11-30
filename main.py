ed73ae6a664a6adb083435c636c60826244658bc
Jaeyeong Cheon <chun3720@hotmail.com>
2022-11-30 19:09:12 +0900

    initial upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_wolrd():
+    name = os.environ.eg("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port = int(os.environ.get("PORT", 8080))
